2bac07932ceb2d903349c62ea4c7511be6c51860
mohit uprim <mohit uprim>
2017-06-01 23:09:21 +0530

    Data Binding & Pipes

--- app/app.module.js
+++ app/app.module.js
@@ -8,6 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
+//required for two binding
+var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
 var product_list_component_1 = require("./products/product-list.component");
 var AppModule = (function () {
@@ -17,7 +19,9 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule],
+        //External module
+        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
+        //Internal Module
         declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent],
         bootstrap: [app_component_1.AppComponent]
     })
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//required for two binding
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import {ProductListComponent} from './products/product-list.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  //External module
+  imports: [ BrowserModule, FormsModule ],
+  //Internal Module
   declarations: [ AppComponent, ProductListComponent ],
   bootstrap: [ AppComponent ]
 })
