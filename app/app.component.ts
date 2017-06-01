3cb613397b2eb7461fb73031bbb9300997cc7245
mohit uprim <mohit uprim>
2017-06-01 21:56:04 +0530

    Introduction to components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+//Decorator to supply metadata
+//class becomes component with component decorator
 @Component({
+    //Directive name used int HTML
     selector: 'pm-app',
+    //View Layout
     template: '<h1>Angular2: Getting Started</h1>'
 })
+//Class
 export class AppComponent { }
