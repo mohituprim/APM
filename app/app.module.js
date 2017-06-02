843221716307983bef5a35df37eb2bfe4bc64d92
mohit uprim <mohit uprim>
2017-06-02 12:23:48 +0530

    Nested Component

--- app/app.module.js
+++ app/app.module.js
@@ -13,6 +13,7 @@ var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
 var product_list_component_1 = require("./products/product-list.component");
 var product_filter_pipe_1 = require("./products/product-filter.pipe");
+var star_component_1 = require("./shared/star.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -23,7 +24,7 @@ AppModule = __decorate([
         //External module
         imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
         //Internal Module
-        declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent, product_filter_pipe_1.ProductFilterPipe],
+        declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent, product_filter_pipe_1.ProductFilterPipe, star_component_1.StarComponent],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 //required for two binding
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent }  from './app.component';
 import {ProductListComponent} from './products/product-list.component';
 import {ProductFilterPipe} from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   //External module
   imports: [ BrowserModule, FormsModule ],
   //Internal Module
-  declarations: [ AppComponent, ProductListComponent, ProductFilterPipe ],
+  declarations: [ AppComponent, ProductListComponent, ProductFilterPipe, StarComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
